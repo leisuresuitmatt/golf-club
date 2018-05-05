2feb44a17456a5bf1474581a77434a5ca66e2fce
Maciek <mac.w.brzezinski@gmail.com>
2018-05-05 21:15:24 +0200

    some cleanup
    + build settings

--- Assets/scripts/PlayerSelectManager.cs
+++ Assets/scripts/PlayerSelectManager.cs
@@ -8,7 +8,7 @@ public class PlayerSelectManager : MonoBehaviour
 {
 
     public Text[] plReadyTexts = new Text[4];
-    
+
     public Text golf;
     public string keyboard;
     public string controler1;
@@ -37,7 +37,7 @@ public class PlayerSelectManager : MonoBehaviour
 
     public string SceneToLoad;
 
-    void Start ()
+    void Start()
     {
         for (int i = 0; i < plReadyTexts.Length; i++)
         {
@@ -51,9 +51,9 @@ public class PlayerSelectManager : MonoBehaviour
             plEnabled[i] = false;
         }
     }
-	
-	
-	void Update ()
+
+
+    void Update()
     {
         if (Input.GetButtonDown(keyboard + "Jump") && !keyboardSelected)
         {
@@ -65,6 +65,7 @@ public class PlayerSelectManager : MonoBehaviour
                     plEnabled[i] = true;
                     plControls[i] = keyboard;
                     plReadyTexts[i].color = teamcolors[i];
+                    plReadyTexts[i].text = "Keyboard";
                     plTeams[i] = i;
                     keyboardIndex = i;
                     numberOfPlayers++;
@@ -82,6 +83,7 @@ public class PlayerSelectManager : MonoBehaviour
                     plEnabled[i] = true;
                     plControls[i] = controler1;
                     plReadyTexts[i].color = teamcolors[i];
+                    plReadyTexts[i].text = "Controller 1";
                     plTeams[i] = i;
                     controller1Index = i;
                     numberOfPlayers++;
@@ -99,6 +101,7 @@ public class PlayerSelectManager : MonoBehaviour
                     plEnabled[i] = true;
                     plControls[i] = controler2;
                     plReadyTexts[i].color = teamcolors[i];
+                    plReadyTexts[i].text = "Controller 2";
                     plTeams[i] = i;
                     controller2Index = i;
                     numberOfPlayers++;
@@ -116,6 +119,7 @@ public class PlayerSelectManager : MonoBehaviour
                     plEnabled[i] = true;
                     plControls[i] = controler3;
                     plReadyTexts[i].color = teamcolors[i];
+                    plReadyTexts[i].text = "Controller 3";
                     plTeams[i] = i;
                     controller3Index = i;
                     numberOfPlayers++;
@@ -133,12 +137,69 @@ public class PlayerSelectManager : MonoBehaviour
                     plEnabled[i] = true;
                     plControls[i] = controler4;
                     plReadyTexts[i].color = teamcolors[i];
+                    plReadyTexts[i].text = "Controller 4";
                     plTeams[i] = i;
                     controller4Index = i;
                     numberOfPlayers++;
                     break;
                 }
             }
+        }
+
+        if (Input.GetButtonDown(keyboard + "Cancel") && keyboardSelected)
+        {
+            keyboardSelected = false;
+
+            plEnabled[keyboardIndex] = false;
+            plControls[keyboardIndex] = null;
+            plReadyTexts[keyboardIndex].color = new Color(0, 0, 0, 1);
+            plReadyTexts[keyboardIndex].text = "Player " + (keyboardIndex + 1);
+            plTeams[keyboardIndex] = 0;
+            numberOfPlayers--;
+        }
+        if (Input.GetButtonDown(controler1 + "Cancel") && p1selected)
+        {
+            p1selected = false;
+
+            plEnabled[controller1Index] = false;
+            plControls[controller1Index] = null;
+            plReadyTexts[controller1Index].color = new Color(0, 0, 0, 1);
+            plReadyTexts[controller1Index].text = "Player " + (controller1Index + 1);
+            plTeams[controller1Index] = 0;
+            numberOfPlayers--;
+        }
+        if (Input.GetButtonDown(controler2 + "Cancel") && p2selected)
+        {
+            p2selected = false;
+
+            plEnabled[controller2Index] = false;
+            plControls[controller2Index] = null;
+            plReadyTexts[controller2Index].color = new Color(0, 0, 0, 1);
+            plReadyTexts[controller2Index].text = "Player " + (controller2Index + 1);
+            plTeams[controller2Index] = 0;
+            numberOfPlayers--;
+        }
+        if (Input.GetButtonDown(controler3 + "Cancel") && p3selected)
+        {
+            p3selected = false;
+
+            plEnabled[controller3Index] = false;
+            plControls[controller3Index] = null;
+            plReadyTexts[controller3Index].color = new Color(0, 0, 0, 1);
+            plReadyTexts[controller3Index].text = "Player " + (controller3Index + 1);
+            plTeams[controller3Index] = 0;
+            numberOfPlayers--;
+        }
+        if (Input.GetButtonDown(controler4 + "Cancel") && p4selected)
+        {
+            p4selected = true;
+
+            plEnabled[controller4Index] = false;
+            plControls[controller4Index] = null;
+            plReadyTexts[controller4Index].color = new Color(0, 0, 0, 1);
+            plReadyTexts[controller4Index].text = "Player " + (controller4Index + 1);
+            plTeams[controller4Index] = 0;
+            numberOfPlayers--;
         }
 
         if (Input.GetButtonDown(keyboard + "Jump") && keyboardSelected)
@@ -196,8 +257,12 @@ public class PlayerSelectManager : MonoBehaviour
         {
             golf.enabled = true;
         }
+        else
+        {
+            golf.enabled = false;
+        }
 
-        if(golf.enabled && Input.GetKeyDown(KeyCode.Return))
+        if (golf.enabled && Input.GetButtonDown("Start"))
         {
             for (int i = 0; i < plTeams.Length; i++)
             {
