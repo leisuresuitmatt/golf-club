189602f94a3cc1ff4fb2b96b2f6fbd5904c21937
Anntoja <averse@autograf.pl>
2018-05-27 19:41:14 +0200

    UI setup; change font; add dot for players

--- Assets/scripts/UImachine.cs
+++ Assets/scripts/UImachine.cs
@@ -9,10 +9,12 @@ public class UImachine : MonoBehaviour
     public GolfHit player;
     public Text hp;
     public Text power;
+    public Slider slider;
 	
 	void Update ()
     {
         hp.text = "HP: " + playerhealth.hp;
         power.text = "Power: " + player.powerBuildUp*100 + "%";
-	}
+
+    }
 }
